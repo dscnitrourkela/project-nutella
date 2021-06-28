e303bd99b23006c626f9e4cf17a320208fddd72e
Ritesh Patil <riteshsp2000@gmail.com>
2021-06-28 23:10:37 +0530

    feat: Create User model
    - Create mongoose model, typescript interface, graphql object id

--- src/schema/user/user.model.ts
+++ src/schema/user/user.model.ts
@@ -1,27 +1,52 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 // Libraries
+import {Field, ObjectType, ID} from 'type-graphql';
 import {prop as Property, getModelForClass} from '@typegoose/typegoose';
-import {Field as GqlField, ObjectType as GqlObjectType} from 'type-graphql';
+import {ObjectId} from 'mongodb';
 
-@GqlObjectType()
+@ObjectType({description: 'The User Model'})
 export class User {
-  @GqlField()
-  readonly _id!: string;
+  @Field(() => ID, {description: 'MongoDB ObjectID'})
+  id: ObjectId;
 
-  @Property({default: new Date()})
-  @GqlField(() => Date)
-  createdAt: Date;
-
-  @Property({default: new Date()})
-  @GqlField(() => Date)
-  updatedAt: Date;
-
-  @Property()
-  @GqlField(() => String)
-  username: string;
-
-  @Property()
-  @GqlField(() => String)
+  @Property({required: true, trim: true})
+  @Field({description: 'Name of the user'})
   name: string;
+
+  @Property({
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    match:
+      /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@gmail(.([^<>()[\].,;:\s@"]{2,})){1,}$/i,
+  })
+  @Field({description: 'Unique email of the user'})
+  email: string;
+
+  @Property({required: true, unique: true, trim: true})
+  @Field({description: 'Unique phoneNo of the user'})
+  phoneNo: string;
+
+  @Property({required: true, unique: true, trim: true})
+  @Field({description: 'Institute roll number of the student'})
+  rollNo: string;
+
+  @Property({default: []})
+  @Field(() => [String], {
+    description: 'An array of the fcm tokens of user devices',
+  })
+  fcmToken: string[];
+
+  @Property({required: true, unique: true})
+  @Field({description: 'Firebase ID of the user'})
+  uid: string;
+
+  @Property({default: []})
+  @Field({description: 'An array of IDs of the quiz'})
+  quiz: string[];
+
+  // Todo resolve the quiz from the id after creation of quiz model
 }
 
 export const UserModel = getModelForClass(User);
