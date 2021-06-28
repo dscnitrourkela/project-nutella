df57efa681b2b7801c3682c3cf197fec5dd2aa39
Ritesh Patil <riteshsp2000@gmail.com>
2021-06-28 22:41:30 +0530

    minor: Fixes in user.model.ts

--- src/schema/user/user.model.ts
+++ src/schema/user/user.model.ts
@@ -1,17 +1,17 @@
 // Libraries
-import { prop as Property, getModelForClass } from "@typegoose/typegoose";
-import { Field as GqlField, ObjectType as GqlObjectType } from "type-graphql";
+import {prop as Property, getModelForClass} from '@typegoose/typegoose';
+import {Field as GqlField, ObjectType as GqlObjectType} from 'type-graphql';
 
 @GqlObjectType()
 export class User {
   @GqlField()
   readonly _id!: string;
 
-  @Property({ default: new Date() })
+  @Property({default: new Date()})
   @GqlField(() => Date)
   createdAt: Date;
 
-  @Property({ default: new Date() })
+  @Property({default: new Date()})
   @GqlField(() => Date)
   updatedAt: Date;
 
