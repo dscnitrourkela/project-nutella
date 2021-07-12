8987c1a007aad34c71045892852f9be37f0e722b
Ritesh Patil <riteshsp2000@gmail.com>
2021-07-12 23:27:23 +0530

    deploy: create a function to determine allowed domains for CORS error

--- src/constants.ts
+++ src/constants.ts
@@ -2,8 +2,11 @@ export const PORT = process.env.PORT || 8000;
 export const {MONGO_APP_URL} = process.env;
 
 export const IS_PROD = process.env.NODE_ENV === 'production';
-export const PROD_ORIGIN = 'prod.url';
-export const DEV_ORIGIN = 'http://localhost:3000';
+export const PROD_ORIGIN = 'https://aptiche-admin.dscnitrourkela.org';
+export const DEV_ORIGIN = 'http://localhost';
+export const STAGING_ORIGIN =
+  'https://aptiche-admin-staging.dscnitrourkela.org';
+export const PR_ORIGIN = 'https://apti-che';
 
 export const PERMISSIONS = {
   USER: 'user',
