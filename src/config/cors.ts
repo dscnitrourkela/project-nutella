537b81435f7189cf8ec343575d7669091f5db68a
Ritesh Patil <riteshsp2000@gmail.com>
2021-07-12 23:27:23 +0530

    deploy: create a function to determine allowed domains for CORS error

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,6 +1,29 @@
-import {IS_PROD, PROD_ORIGIN, DEV_ORIGIN} from '../constants';
+import {
+  IS_PROD,
+  PROD_ORIGIN,
+  DEV_ORIGIN,
+  PR_ORIGIN,
+  STAGING_ORIGIN,
+} from '../constants';
 
 export default {
-  origin: IS_PROD ? PROD_ORIGIN : DEV_ORIGIN,
   credentials: true,
+  origin: (
+    origin: string,
+    callback: (error: null | Error, success?: boolean) => void,
+  ): void => {
+    if (
+      !IS_PROD &&
+      (!origin ||
+        origin.includes(DEV_ORIGIN) ||
+        origin.includes(PR_ORIGIN) ||
+        origin === STAGING_ORIGIN)
+    ) {
+      callback(null, true);
+    } else if (origin === PROD_ORIGIN) {
+      callback(null, true);
+    } else {
+      callback(new Error('Request blocked by CORS. Invalid source!'));
+    }
+  },
 };
