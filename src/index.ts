d30cabd16e457534e54ea7bab4f1afb93eeb52a4
Abhibhaw Asthana <39991296+abhibhaw@users.noreply.github.com>
2021-07-12 21:36:32 +0530

    Revert "feat: Setup express session with mongodb store"
    This reverts commit 1b2703dd717e36e17775344b40c9a05b8000b0a5.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 import 'reflect-metadata';
 import 'dotenv/config';
 
@@ -6,7 +5,6 @@ import 'dotenv/config';
 import {ApolloServer} from 'apollo-server-express';
 import express from 'express';
 import cors from 'cors';
-import Session from 'express-session';
 
 // Config
 import {init as initMongoose} from './config/mongoose';
@@ -22,54 +20,20 @@ import winston from './config/winston';
 // Constants
 import {PORT, IS_PROD} from './constants';
 
-// eslint-disable-next-line import/order
-const MongoDBStore = require('connect-mongodb-session')(Session);
-
 (async () => {
-  // Configure Utilities
   const logger = winston('Express');
   initMongoose();
   initFirebase();
 
-  // Initialize Express Application
   const app = express();
   app.use(cors(CORS_OPTIONS));
 
-  // Initialize Session
-  const store = new MongoDBStore({
-    uri: process.env.MONGO_APP_URL,
-    collection: 'sessionStore',
-    expires: 3600000, // 1 Hour
-    connectionOptions: {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      poolSize: 100,
-      serverSelectionTimeoutMS: 10000,
-    },
-  });
-  store.on('error', (error: unknown) => {
-    logger.error(`Error on Session Store`, error);
-  });
-  app.use(
-    Session({
-      secret: process.env.SESSION_SECRET,
-      cookie: {
-        maxAge: 86400000,
-      },
-      store,
-      resave: true,
-      saveUninitialized: true,
-    }),
-  );
-
-  // Initialize Apollo Server
   const apolloServer = new ApolloServer({
     schema: await schema,
     playground: !IS_PROD,
     debug: !IS_PROD,
   });
 
-  // Configure Express with Apollo Server
   apolloServer.applyMiddleware({
     app,
     cors: CORS_OPTIONS,
