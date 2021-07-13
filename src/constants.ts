9be66aa7e22d5157eecb5ad57a6a6a80088fe27b
Ritesh Patil <riteshsp2000@gmail.com>
2021-07-13 21:56:20 +0530

    fix: minor cors config fix in development mode

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,10 @@ export const {MONGO_APP_URL} = process.env;
 
 export const IS_PROD = process.env.NODE_ENV === 'production';
 export const PROD_ORIGIN = 'https://aptiche-admin.dscnitrourkela.org';
-export const DEV_ORIGIN = 'http://localhost';
+export const DEV_ORIGIN = 'http://localhost:3000';
+export const DEV_ORIGIN_IP = 'http://127.0.0.1:3000';
+export const DEV_SOURCE = 'http://localhost:8000';
+export const DEV_SOURCE_IP = 'http://127.0.0.1:8000';
 export const STAGING_ORIGIN =
   'https://aptiche-admin-staging.dscnitrourkela.org';
 export const PR_ORIGIN = 'https://apti-che';
